81b006a86b5db99c2353936df2c479aed3e79efc
dayanalily <dayanalily22@gmail.com>
2023-09-15 17:34:40 -0300

    test webpack script

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ import * as styles from '../components/stories/assets/css/styles.css'
 
 export { styles };
 
-export * from '../components/index.js';
+// export * from '../components/index.js';
+
+
+import RegButton from '../components/stories/Button';
+import RegDateRangePicker from '../components/stories/DateRangePicker';
+import RegInput from '../components/stories/Input';
+
+export {
+    RegButton,
+    RegDateRangePicker,
+    RegInput
+};
